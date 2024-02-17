fab79e3ec114bef496ab4c743de6f58b65f36f04
Javier <cjavier.gar@gmail.com>
2024-02-16 20:08:16 -0600

    action de obtener datos

--- server.js
+++ server.js
@@ -18,38 +18,82 @@ app.get('/', (req, res) => {
 
 app.post('/registrar-transacciones', (req, res) => {
   const { transactions } = req.body;
-
-  // Mapear transacciones al formato de Airtable
-  const recordsToCreate = transactions.map(transaction => ({
-      fields: {
-          "Concepto": transaction.description,
-          "Fecha de Transacción": transaction.date,
-          "Cantidad": transaction.amount
-      }
-  }));
-
-  base('Estado de cuenta').create(recordsToCreate, function(err, records) {
-    if (err) {
-        console.error(err);
-        return res.status(500).json({ message: "Error al registrar transacciones en Airtable", error: err });
-    }
-    res.status(200).json({ message: "Transacciones registradas con éxito en Airtable", records });
+  registerTransactions(transactions)
+      .then(records => {
+          res.status(200).json({ message: "Transacciones registradas con éxito en Airtable", records });
+      })
+      .catch(err => {
+          console.error(err);
+          res.status(500).json({ message: "Error al registrar transacciones en Airtable", error: err.message });
+      });
 });
+
+// Endpoint para obtener datos por rango de fechas
+app.post('/datos-por-fecha', (req, res) => {
+  const { fechaInicial, fechaFinal } = req.body;
+
+  getAirtableTransactions(fechaInicial, fechaFinal)
+      .then(data => {
+          res.status(200).json(data);
+      })
+      .catch(err => {
+          console.error(err);
+          res.status(500).json({ message: "Error al obtener datos de Airtable", error: err });
+      });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
 function registerTransactions(transactions) {
-    // Aquí implementarías la lógica para procesar y almacenar las transacciones
-    // Por ejemplo, podrías iterar sobre el arreglo de transacciones y guardarlas una por una
-  
-    transactions.forEach(transaction => {
-      // Simulando el registro de cada transacción
-      console.log("Registrando transacción:", transaction);
+  return new Promise((resolve, reject) => {
+      // Mapear transacciones al formato de Airtable
+      const recordsToCreate = transactions.map(transaction => ({
+          fields: {
+              "Concepto": transaction.description,
+              "Fecha de Transacción": transaction.date,
+              "Cantidad": transaction.amount
+          }
+      }));
+
+      base('Estado de cuenta').create(recordsToCreate, (err, records) => {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(records);
+          }
+      });
+  });
+}
+
+
+  function getAirtableTransactions(fechaInicial, fechaFinal) {
+    return new Promise((resolve, reject) => {
+        // Ajustar la fecha final para incluir el día completo
+        let fechaFinalObj = new Date(fechaFinal);
+        fechaFinalObj.setDate(fechaFinalObj.getDate() + 1);
+        let fechaFinalAjustada = fechaFinalObj.toISOString().split('T')[0];
+
+        const filterByFormula = `AND(IS_AFTER({Fecha de Transacción}, '${fechaInicial}'), IS_BEFORE({Fecha de Transacción}, '${fechaFinalAjustada}'))`;
+
+        console.log(`Fórmula de Filtro: ${filterByFormula}`);
+
+        base('Estado de cuenta').select({
+            filterByFormula,
+            view: "Transacciones"
+        }).firstPage((err, records) => {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(`Registros Recibidos: `, records);
+                const data = records.map(record => ({
+                    id: record.id,
+                    fields: record.fields
+                }));
+                resolve(data);
+            }
+        });
     });
-  
-    // Retorna algún resultado, como el número de transacciones procesadas
-    return { processed: transactions.length };
-  }
+}
